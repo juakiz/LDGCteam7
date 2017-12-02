e982a220b7ee4834bd772604eb25b89e83178a60
juakiz <juakiz@gmail.com>
2017-12-02 19:33:58 +0100

    -pollo loaded

--- npm/src/groups/backgroundGP.js
+++ npm/src/groups/backgroundGP.js
@@ -13,6 +13,11 @@ export default class extends Phaser.Group {
     this.piedra1 = game.add.sprite(1280/2, 720, 'piedra_1');
     this.piedra1.anchor.set(0.5,1)
 
+    this.torreLeft = game.add.sprite(0, this.game.world.height, 'torre_vikingos_cortada');
+    this.torreLeft.anchor.set(0,1);
+    this.torreRight = game.add.sprite(this.game.world.width, this.game.world.height, 'torre_vikingos_cortada');
+    this.torreRight.anchor.set(1,1);
+    
     this.add(this.bg);
     this.add(this.piedra1);
 
--- npm/src/groups/base_piedraGP.js
+++ npm/src/groups/base_piedraGP.js
@@ -12,6 +12,7 @@ export default class extends Phaser.Group {
 
     this.leftWeight = 0;
     this.rightWeight = 0;
+    this.balance = 0;
 
     console.log(this._state);
     this.init()
@@ -24,7 +25,21 @@ export default class extends Phaser.Group {
     this.base_piedra.anchor.set(0.5, 0.6);
     // console.log(this.base_piedra.width);
 
+    this.pollo = game.add.sprite(0, -this.base_piedra.height * this.base_piedra.anchor.y, 'pollo_ataque');
+    this.pollo.anchor.set(0.5,1);
+    this.pollo.animations.add('pollo_ataque');
+    this.pollo.animations.add('pollo_caminar');
+    this.KeyA = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+    this.KeyZ = this.game.input.keyboard.addKey(Phaser.Keyboard.Z);
+    this.KeyA.onDown.add(()=>{this.pollo.animations.play('pollo_ataque', 10);});
+    this.KeyZ.onDown.add(()=>{this.pollo.animations.play('pollo_caminar', 10);});
+    
+    
+    // this.pollo.animations.play('attack', 10, true);
+
+
     this.add(this.base_piedra);
+    this.add(this.pollo);
 
     // this.angle = -30;
     // this.game.add.tween(this).to({ angle: '+60'}, 1000, Phaser.Easing.Sinusoidal.InOut, true, 0, -1, true);
@@ -49,11 +64,12 @@ export default class extends Phaser.Group {
   {
     this.rightWeight = amount;    
   }
-  addU
-  nit()
+  
+  setBalance()
   {
-
+    this.balance = this.rightWeight - this.leftWeight;
   }
+
 
   update () {
     // this.angle += 1
