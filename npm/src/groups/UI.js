30775024bb78294ee42c8a1ef76ddcf6b1a16487
yacoanix <aymedeyacoan@gmail.com>
2017-12-02 19:06:05 +0000

    torre_alien

--- npm/src/groups/UI.js
+++ npm/src/groups/UI.js
@@ -8,9 +8,21 @@ export default class extends Phaser.Group {
     this.init()
   }
 
+  
   init()
   {
     this.ui = game.add.sprite(0, 0, 'ui');
     this.add(this.ui);
+    this.debugKeyQ = this.game.input.keyboard.addKey(Phaser.Keyboard.Q);
+    this.debugKeyQ.onDown.add(this.pene, this);
+
+  }
+
+  pene()
+  {
+    //var qKey;
+    //qKey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
+    game.camera.shake(0.05, 500);
+    //  this.game.add.tween(this).to({ x: '+10', y: '+10'}, 100, Phaser.Easing.Sinusoidal.InOut, true, 0, 0, true);
   }
 }
